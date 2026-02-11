882e7282b6d63e671320e8a88988e9c5a2f9dd39
Abian Suarez <abiansc@hotmail.com>
2026-02-11 20:40:36 +0000

    Esqueleto del proyecto CISNE

---
+++ backend/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "Ab1anm88",
+  DB: "db_cisne",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
