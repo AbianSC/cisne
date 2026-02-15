a3e343a2f4f2bb838bc8279b0cf2c112ee30d9d8
FSHIS2 <tc.estadistica@gmail.com>
2026-02-15 21:02:10 +0000

    fix: models/index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,24 +1,44 @@
 const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, 
-  dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
 
-    pool: {
-      max: dbConfig.pool.max,
-      min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
-      idle: dbConfig.pool.idle
-    }
-  });
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
 
-  const db= {};
+const db = {};
 
-  db.Sequelize = Sequelize;
-  db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-  db.cisne = require("./cisne.model.js")(sequelize, Sequelize);
+// Importar todos los modelos desde cisne.model.js
+const models = require("./cisne.model.js");
 
-  module.exports = db;
+// Asignar los modelos al objeto db
+db.User = models.User;
+db.Invoice = models.Invoice;
+db.ServiceInvoice = models.ServiceInvoice;
+db.CourseInvoice = models.CourseInvoice;
+db.Service = models.Service;
+db.Course = models.Course;
+db.Centre = models.Centre;
+db.Therapist = models.Therapist;
+db.Patient = models.Patient;
+db.Resource = models.Resource;
+db.Employs = models.Employs;
+db.Treats = models.Treats;
+db.Pays = models.Pays;
+db.Posts = models.Posts;
+db.Buys = models.Buys;
+db.Publish = models.Publish;
+db.Consume = models.Consume;
+db.Creates = models.Creates;
+db.Generates = models.Generates;
+
+module.exports = db;
